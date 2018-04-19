7de0fd4c1373abc7b9678f6a2e4ba29306c4d967
Sankalp Patro <Sankalp@Sankalps-MBP.wv.cc.cmu.edu>
2018-04-19 03:37:02 -0400

    Added Model_select, not completely finished, because task wasn't understood

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -8,3 +8,20 @@ generate_data = function(n, p)
   }
   return(list(mat = cov, responses = response))
 }
+
+model_select = function(covariates, responses, cutoff)
+{
+  index = c()
+  for (i in 1:nrow(covariates))
+  {
+    if(all(covariates[i] <= cutoff))
+    {
+      index = c(index, i)
+    }
+  }
+  new_cov = covariates[index,]
+  new_res = responses[index]
+  df = data.frame(cov = new_cov, res = new_res)
+  model = lm(formula = res ~ cov, data = df)
+  return(model)
+}
