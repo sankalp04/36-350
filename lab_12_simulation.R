0be63da09e2baf9d20b1f7fa113de3b201417267
Sankalp Patro <Sankalp@Sankalps-MBP.wv.cc.cmu.edu>
2018-04-19 03:10:32 -0400

    Defined generate_data

---
+++ lab_12_simulation.R
@@ -0,0 +1,10 @@
+generate_data = function(n, p)
+{
+  cov = matrix(nrow = n, ncol = p)
+  response = rnorm(n)
+  for (i in 1:n)
+  {
+    cov[i,] = rnorm(p)
+  }
+  return(list(mat = cov, responses = response))
+}
