d4f89401280dac1aaf9d9ea70daf6ee94f28898e
Sankalp Patro <Sankalp@Sankalps-MBP.wv.cc.cmu.edu>
2018-04-19 13:25:15 -0400

    Fixed model_select.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -9,19 +9,26 @@ generate_data = function(n, p)
   return(list(covariates = cov, responses = response))
 }
 
+
+
 model_select = function(covariates, responses, cutoff)
 {
-  index = c()
-  for (i in 1:nrow(covariates))
+  index = c() #storing response column index
+  df = data.frame(cov = covariates, res = responses)
+  model = lm(formula = res ~ cov.1 + cov.2 + cov.3 + cov.4 + cov.5 + cov.6, data = df)
+  summ = model.summary(model)
+  for (i in 23:28)
   {
-    if(all(covariates[i] <= cutoff))
+    if (summ[[4]][i] <= cutoff) 
     {
-      index = c(index, i)
+      index = c(index, (i - 22)) #index of the column to keep
     }
   }
-  new_cov = covariates[index,]
-  new_res = responses[index]
-  df = data.frame(cov = new_cov, res = new_res)
-  model = lm(formula = res ~ cov, data = df)
-  return(model)
+  if (length(index) == 0)
+  {
+    return(index)
+  }
+  lm_new = lm(formula = L$responses ~ L$covariates[, index])
+  mod.sum = model.summary(df_new)
+  return(mod.sum$coefficients[,4])
 }
